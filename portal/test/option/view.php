6ab10fd7ad97b0c9feebbc1a105a28ca6e66c2d4
Ahmad Karimi <ahmadkarimi1991@gmail.com>
2014-11-12 11:56:45 +0330

    add cards feature to portal

--- portal/test/option/view.php
+++ portal/test/option/view.php
@@ -3,14 +3,6 @@
  * @author reza mohiti rm.biqarar@gmail.com
  */
 class view extends main_view{
-	public function config(){
-		$this->global->page_title = "group";
-		$this->global->url = $this->uStatus(true);
-		$f = $this->form('@group', $this->uStatus());
-
-		$this->sql(".edit", "group", $this->uId(), $f);
-		// $this->sql("#a");
-		// exit();
-		}
+	public function config(){}
 }
 ?>
