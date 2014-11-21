ecff8fc802e6824c95dacfcea7af77e20f92f282
Reza <rm.biqarar@gmail.com>
2014-11-21 19:28:51 +0330

    dataTable classes list

---
+++ portal/classes/list/model.php
@@ -0,0 +1,11 @@
+<?php 
+
+/**
+* 
+*/
+class model extends main_model {
+		public function post_api(){
+		$data = $this->sql(".dataTable", "classes", false, "name, statr_date, end_date");
+	}	
+}
+?>
--- portal/classes/list/view.php
+++ portal/classes/list/view.php
@@ -9,51 +9,51 @@ class view extends main_view{
 		//------------------------------ global
 		$this->global->page_title = "classes";
 
-		//------------------------------ classes list
-		$classes_detail = $this->sql(".list", "classes", function($query) {
-			$query->limit(80);
-		})
+		// //------------------------------ classes list
+		// $classes_detail = $this->sql(".list", "classes", function($query) {
+		// 	$query->limit(80);
+		// })
 
-		//------------------------------ detail link
-		->addCol("detail", "description")
-		->select(-1, "detail")
-		->html($this->detailLink("classes"))
+		// //------------------------------ detail link
+		// ->addCol("detail", "description")
+		// ->select(-1, "detail")
+		// ->html($this->detailLink("classes"))
 
-		//------------------------------ edit link
-		->addCol("edit", "edit")
-		->select(-1, "edit")
-		->html($this->editLink("classes"));
+		// //------------------------------ edit link
+		// ->addCol("edit", "edit")
+		// ->select(-1, "edit")
+		// ->html($this->editLink("classes"));
 
-		//------------------------------ set type of list (absence | classification)
-		$type = (isset(config_lib::$surl['type'])) ? config_lib::$surl['type'] : "classification";
+		// //------------------------------ set type of list (absence | classification)
+		// $type = (isset(config_lib::$surl['type'])) ? config_lib::$surl['type'] : "classification";
 
-		if($type == "absence"){
+		// if($type == "absence"){
 
-			//------------------------------ absence link
-			$classes_detail = $classes_detail->addCol("absence","absence")
-			->select(-1, "absence")
-			->html($this->link("classification/absence/classesid=%id%", "href", "ico icoabsence"));
+		// 	//------------------------------ absence link
+		// 	$classes_detail = $classes_detail->addCol("absence","absence")
+		// 	->select(-1, "absence")
+		// 	->html($this->link("classification/absence/classesid=%id%", "href", "ico icoabsence"));
 
-		}else{
-			//------------------------------ classification link
-			$classes_detail = $classes_detail->addCol("classification","classification")
-			->select(-1, "classification")
-			->html($this->link("classification/class/classesid=%id%", "href", "ico icoclasses"));
+		// }else{
+		// 	//------------------------------ classification link
+		// 	$classes_detail = $classes_detail->addCol("classification","classification")
+		// 	->select(-1, "classification")
+		// 	->html($this->link("classification/class/classesid=%id%", "href", "ico icoclasses"));
 			
-			//------------------------------ print link
-			$classes_detail = $classes_detail->addCol("print", "print")
-			->select(-1, "print")
-			->html($this->link("classification/printlist/classesid=%id%", "href" , "ico icoletters a-undefault"));
-		}
+		// 	//------------------------------ print link
+		// 	$classes_detail = $classes_detail->addCol("print", "print")
+		// 	->select(-1, "print")
+		// 	->html($this->link("classification/printlist/classesid=%id%", "href" , "ico icoletters a-undefault"));
+		// }
 
-		//------------------------------ compile sql.list
-		$classes_detail = $classes_detail->compile();
+		// //------------------------------ compile sql.list
+		// $classes_detail = $classes_detail->compile();
 		
-		//------------------------------ convert paln_id , teacher , place id , ... to name of this
-		$classes_detail = $this->detailClasses($classes_detail);
+		// //------------------------------ convert paln_id , teacher , place id , ... to name of this
+		// $classes_detail = $this->detailClasses($classes_detail);
 		
 
-		$this->data->list = $classes_detail;
+		// $this->data->list = $classes_detail;
 	}
 }
 ?>
