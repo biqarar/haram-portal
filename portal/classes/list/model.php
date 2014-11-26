3773dc68d1fbd589adf934817463b9b1df161770
Reza <rm.biqarar@gmail.com>
2014-11-26 23:04:01 +0330

    f

--- portal/classes/list/model.php
+++ portal/classes/list/model.php
@@ -5,7 +5,9 @@
 */
 class model extends main_model {
 		public function post_api(){
-		$data = $this->sql(".dataTable", "classes", false, "name, statr_date, end_date");
+		// $data = $this->sql(".dataTable", "classes", false, "name, statr_date, end_date");
+		// var_dump($data);
+		// die();
 	}	
 }
 ?>
