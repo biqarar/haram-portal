53dd23bf7c2dd4da37b5f0b58ff082fabce2c7c4
baravak <itb.baravak@gmail.com>
2014-12-15 14:24:14 +0330

    f

--- portal/education/list/model.php
+++ portal/education/list/model.php
@@ -3,7 +3,7 @@ class model extends main_model {
 	public function post_api() {
 		$dtable = $this->dtable->table("education")
 			->fields("id", "group", "section", "id edit")
-			->search_fields("‍‍‍group", "section")
+			->search_fields("`group`", "section")
 			->result(function($r){
 				$r->edit = '<a href="education/status=edit/id=' . $r->edit . '" class="icoedit" ></a>';
 			});
