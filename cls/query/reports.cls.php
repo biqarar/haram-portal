81f45e24945151db44b5663bd5c2a18b5371534b
Reza <rm.biqarar@gmail.com>
2015-02-14 20:04:51 +0330

    report, classes, person

--- cls/query/reports.cls.php
+++ cls/query/reports.cls.php
@@ -5,5 +5,48 @@ class query_reports_cls extends query_cls
 		$list  = $this->sql()->tableReport()->whereTables($tablse)->select()->allAssoc();
 		return $list;
 	}
+
+	public function person_classes($classes_id = false, $condition = "id=0") {
+
+		$classification = $this->sql()->tableClassification()->whereClasses_id($classes_id)
+		->groupOpen()
+			->condition("and", "#date_delete" , "is", "#null")
+			->condition("or", "#because", "is", "#null")
+		->groupClose();
+
+		$condition = preg_match("/^(.*)\=(.*)$/", $condition, $where);
+	
+		$where_ = "and" . ucfirst($where[1]);
+
+		$classification->joinPerson()->whereUsers_id("#classification.users_id")->$where_($where[2]);
+
+		return $classification->select()->num();
+	}
+
+	public function classes_average_age($classes_id = false) {
+
+		$classification = $this->sql()->tableClassification()->whereClasses_id($classes_id)
+		->groupOpen()
+			->condition("and", "#date_delete" , "is", "#null")
+			->condition("or", "#because", "is", "#null")
+		->groupClose();
+		$classification->joinPerson()->whereUsers_id("#classification.users_id")->fieldBirthday();
+
+		$classification = $classification->select()->allAssoc();
+
+		$age = 0;
+		$sum = 0;
+		// var_dump($classification); exit();
+		foreach ($classification as $key => $value) {
+			if(preg_match("/^(\d{4})(\d{4})$/", $value['birthday'], $y)) {
+				$age += $y[1];
+				$sum++;
+			}
+		}
+
+		return round($age / $sum);
+
+
+	}
 }
 ?>
--- portal/report/classes/person/model.php
+++ portal/report/classes/person/model.php
@@ -11,15 +11,61 @@ class model extends main_model {
 
 		if(!$classesid || empty($classes_id)) return false;
 		
-		$classification = $this->sql()->tableClassification()
+		$reprot = array();
+		foreach ($classes_id as $key => $value) {
+			
+			$classes_detail = $this->sql()->tableClasses()->whereId($value)
+				->fieldStart_time()
+				->fieldEnd_time()
+				->fieldStart_date()
+				->fieldEnd_date()
+				->fieldWeek_days()
+				->fieldCount();
+
+			$classes_detail->joinPerson()->whereUsers_id("#classes.teacher")->fieldName("teacherName")->fieldFamily("teacherFamily");
+			
+			$classes_detail->joinPlan()->whereId("#classes.plan_id")->fieldName("plan");
 		
+			$classes_detail->joinPlace()->whereId("#classes.place_id")->fieldName("place");
+	
+			$classes_detail = $classes_detail->limit(1)->select()->assoc();
+		
+			$classes_detail['male'] = $this->sql(".reports.person_classes", $value, "gender=male");
+			
+			$classes_detail['female'] = $this->sql(".reports.person_classes", $value, "gender=female");
+
+			$classes_detail['single'] = $this->sql(".reports.person_classes", $value, "marriage=single");
+			
+			$classes_detail['married'] = $this->sql(".reports.person_classes", $value, "marriage=married");
+			
+			$classes_detail['average_age'] = $this->sql(".reports.classes_average_age", $value);
+
+
+			$reprot[$key] = $classes_detail;
+
+		}
+
+		return $reprot;
+		var_dump($reprot);
+		exit();
+		//------------------------------ find lis of person in classes
+		$classification = $this->sql()->tableClassification()
+		->groupOpen()
+			->condition("and", "#date_delete" , "is", "#null")
+			->condition("or", "#because", "is", "#null")
+		->groupClose()
 		->groupOpen();
+
+		//------------------------------ all classes_id (is called)
 		foreach ($classes_id as $key => $value) {
 			$classification->orClasses_id($value);
 		}
+		
 		$classification->groupClose()->fieldId();
 
+		//------------------------------ find urers
 		$classification->joinUsers()->whereId("#classification.users_id")->fieldUsername();
+
 
 		// $classification->joinPerson()->whereUsers_id("#classification.users_id")->fieldName("name")->fieldFamily("family");
 
@@ -30,11 +76,11 @@ class model extends main_model {
 			->fieldEnd_date()
 			->fieldWeek_days();
 
-		$classification->joinPerson()->whereUsers_id("#classes.teacher")->fieldName("teacherName")->fieldFamily("teacherFamily");
-
 		$classification->joinPlan()->whereId("#classes.plan_id")->fieldName("plan");
 		
 		$classification->joinPlace()->whereId("#classes.place_id")->fieldName("place");
+
+		$classification->joinPerson()->whereUsers_id("#classification.users_id");
 		
 		
 		
