0b0247f3df5c1d4fef4ff9cc81e04efb02b1d877
Reza <rm.biqarar@gmail.com>
2015-02-22 23:06:33 +0330

    x

--- portal/database/admin/model.php
+++ portal/database/admin/model.php
@@ -108,22 +108,22 @@ class model extends main_model{
 		//---------------------------------------------------------------------------------------------------
 		//---------------------------------------------------------------------------------------------------
 		//---------------------------------------------------------------------------------------------------
-		$classification = $this->sql()->tableClassification();
+		// $classification = $this->sql()->tableClassification();
 
-		$classification->joinBridge()->whereUsers_id("#classification.users_id")->andTitle("mobile");
-		$classification = $classification->select()->allAssoc();
+		// $classification->joinBridge()->whereUsers_id("#classification.users_id")->andTitle("mobile");
+		// $classification = $classification->select()->allAssoc();
 		
-		header('Content-Type: text/html; charset=utf-8'); 
-        header("Content-Disposition: attachment; filename=1.xlsx");  
-        header("Pragma: no-cache"); 
-        header("Expires: 0");
+		// header('Content-Type: text/html; charset=utf-8'); 
+  //       header("Content-Disposition: attachment; filename=1.xlsx");  
+  //       header("Pragma: no-cache"); 
+  //       header("Expires: 0");
 
-		print "<table>";
-		foreach ($classification as $key => $value) {
-			print "<tr><td>" . $value['value'] . "</td></tr>";
-		}
-		print "</table>";
-		exit();
+		// print "<table>";
+		// foreach ($classification as $key => $value) {
+		// 	print "<tr><td>" . $value['value'] . "</td></tr>";
+		// }
+		// print "</table>";
+		// exit();
 		// $this->title("change passwrod");
 		// $all = $this->sql()->tableBridge()->whereTitle("mobile");
 		// $all->joinClassification()->whereUsers_id("#bridge.users_id");
