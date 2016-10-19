2da388a3b61746f156225a75c00f65cac1e14c14
reza <rm.biqarar@gmail.com>
2016-10-19 09:15:11 +0330

    show absece by classe id

--- portal/users/learn/absence/model.php
+++ portal/users/learn/absence/model.php
@@ -7,15 +7,31 @@ class model extends main_model {
 	public function post_api() {
 
 		$dtable = $this->dtable->table("absence")
-		->fields("planname", "teacherfamily", 'date date', 'type',"because", "id edit" , "classificationid delete")
+		->fields("planname","classesid classesid", "teacherfamily", 'date date', 'type',"because", "id edit" , "classificationid delete")
 		->search_fields("date", "classes_id")
 		
 		->query(function($q){
 			//-------------------- check branch
 			$this->sql(".branch.users",$this->xuId("usersid"));
-			
-			$q->joinClassification()->whereId("#absence.classification_id")
-			->andUsers_id($this->xuId("usersid"))->fieldClasses_id("classesid")->fieldId("classificationid");
+			if($this->xuId("classesid") != "0")
+			{
+				
+				$q->joinClassification()
+					->whereId("#absence.classification_id")
+					->andUsers_id($this->xuId("usersid"))
+					->andClasses_id($this->xuId("classesid"))
+					->fieldClasses_id("classesid")
+					->fieldId("classificationid");
+			}
+			else
+			{
+
+				$q->joinClassification()
+					->whereId("#absence.classification_id")
+					->andUsers_id($this->xuId("usersid"))
+					->fieldClasses_id("classesid")
+					->fieldId("classificationid");
+			}
 
 			$q->joinClasses()->whereId("#classification.classes_id")->fieldId("classes_id")->fieldPlan_id("plan_id");
 			$q->joinPlan()->whereId("#classes.plan_id")->fieldName("planname");
@@ -37,6 +53,8 @@ class model extends main_model {
 			$r->edit = '<a class="icoedit" href="absence/status=edit/id='.$r->edit.'" title="'.gettext('edit').' '.$r->edit.'"></a>';
 			$r->delete = $this->tag("a")->class("icoredclose absenceDelete")->date($r->date)
 			->classificationid($r->delete)->render();
+			$r->classesid = $this->tag("a")->vtext($r->classesid)->style("color:blue;")->title("برای نمایش تمامی غیبت های فراگیر در این کلاس کلیک کنید")
+			->href("users/learn/absence/id=". $this->xuId("usersid")."/classesid=". $r->classesid)->render();
 		});
 		$this->sql(".dataTable", $dtable);
 
