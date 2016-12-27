c71ba69b70b8e7f78d22c82526ba6a97a32aab6e
Reza Mohiti <rm.biqarar@gmail.com>
2016-12-27 20:13:33 +0330

    add enum of classification 'move' mode

--- cls/query/absence.cls.php
+++ cls/query/absence.cls.php
@@ -5,18 +5,17 @@ class query_absence_cls extends query_cls
 
 	public function insert($classification = false, $type = false, $date = false)
 	{
-		
+
 		$check = $this->sql()->tableAbsence()
 							->whereClassification_id($classification)
 							->andDate($date)->limit(1)->select()->num();
 
-		if($check == 1) {
-			// $check = $this->sql()->tableAbsence()
-			// 				->whereClassification_id($classification)
-			// 				->andDate($date)->select()->allAssoc();
-			// 				var_dump($check);exit();			
+		if($check == 1)
+		{
 			debug_lib::fatal("برای این فراگیر در این تاریخ غیبت ثبت شده است");
-		}else{
+		}
+		else
+		{
 			//--------------- check if this classification id in the users branch or no.
 			$this->sql(".branch.classification", $classification);
 
@@ -26,21 +25,31 @@ class query_absence_cls extends query_cls
 						->setDate($date)
 						->insert();
 			$this->autoremove($classification, $date);
-			
-		debug_lib::true(_($type) .  " ثبت شد");
+
+			debug_lib::true(_($type) .  " ثبت شد");
 		}
 	}
 
-	public function absence_count($array = false) {
-		
+	/**
+	 * procces the absence code
+	 *
+	 * @param      array|boolean  $array  The array
+	 *
+	 * @return     integer        ( description_of_the_return_value )
+	 */
+	public function absence_count($array = false)
+	{
+
 		$c = 0;
-		foreach ($array as $key => $value) {
+		foreach ($array as $key => $value)
+		{
 			// var_dump($value);
-			switch ($value['type']) {
+			switch ($value['type'])
+			{
 				case 'delay':
 					$c = $c + 0;
 					break;
-				
+
 				case 'leave':
 					$c = $c + 0.5;
 					break;
@@ -56,14 +65,23 @@ class query_absence_cls extends query_cls
 		}
 		return $c;
 	}
-	public function autoremove($classification_id = false, $date = false) {
+
+
+	/**
+	 * auto remove
+	 *
+	 * @param      boolean  $classification_id  The classification identifier
+	 * @param      boolean  $date               The date
+	 */
+	public function autoremove($classification_id = false, $date = false)
+	{
 		// return;
 		$default_date = $date;
 		if(preg_match("/^\d{8}$/", $date))
 		{
 			$date = substr($date,0,4). '-'. substr($date,4,2). '-'. substr($date, 6,2);
 		}
-		// var_dump($date);exit();
+
 		$this->classification_id = $classification_id;
 
 		$max_absence = $this->sql()->tableClassification()->whereId($this->classification_id);
@@ -73,63 +91,74 @@ class query_absence_cls extends query_cls
 								->fieldAbsence_type("absence_type");
 
 		$max_absence = $max_absence->select()->assoc();
-		
+
 		$this->users_id 		= $max_absence['users_id'];
 		$this->classes_id 	= $max_absence['classes_id'];
 		$absence_type 	= $max_absence['absence_type'];
 
-		if($absence_type == "ماهیانه"){
-			
+		if($absence_type == "ماهیانه")
+		{
+
 			$x = preg_split("/\-/", $date);
-		
+
 			$y = (intval($x[0]) < 10) ? "0" . $x[0] : $x[0];
 			$m = (intval($x[1]) < 10) ? "0" . $x[1] : $x[1];
 			$d = (intval($x[2]) < 10) ? "0" . $x[2] : $x[2];
 
 			$start_day = "{$y}{$x[1]}01";
 
-			if($m == "12"){
+			if($m == "12")
+			{
 				$new_m = ($m == '12') ? $new_m = '01' :  intval($m) + 1;
 				$y = intval($y)+1;
-			}else{
+			}
+			else
+			{
 				$new_m = ($m == '12') ? $new_m = '01' :  intval($m) + 1;
 			}
-			
+
 			$new_m = (intval($new_m) < 10) ? "0" . $new_m: $new_m;
-		
 
 			$end_day = "{$y}{$new_m}01";
 
-			
 			$absenc_list = $this->sql()->tableAbsence()
 										->whereClassification_id($this->classification_id)
 										->condition("and", "absence.date", ">=", "$start_day")
 										->condition("and", "absence.date", "<", "$end_day")
 										->select();
-										
+
 			$c = $this->absence_count($absenc_list->allAssoc());
 
 			$this->remove($c, $max_absence['max_absence']);
-
-
-		}else{
+		}
+		else
+		{
 
 			$absence_count = $this->sql()->tableAbsence()->whereClassification_id($this->classification_id)->select();
 
 			$c = $this->absence_count($absence_count->allAssoc());
 
-			$this->remove($c, $max_absence['max_absence']);	
+			$this->remove($c, $max_absence['max_absence']);
 		}
 	}
 
-	public function remove($c =false, $max = false) {
-	
-		if(intval($c) > intval($max)){
+
+	/**
+	 * remove users from classes
+	 *
+	 * @param      boolean  $c      { parameter_description }
+	 * @param      boolean  $max    The maximum
+	 */
+	public function remove($c =false, $max = false)
+	{
+
+		if(intval($c) > intval($max))
+		{
 				$this->sql(".classification.remove",
-							$this->users_id, 
-							$this->classes_id, 
+							$this->users_id,
+							$this->classes_id,
 							$this->classification_id ,
-							"absence" , 
+							"absence" ,
 							$this->dateNow());
 			debug_lib::true("<a style='color:red'>فراگیر به دلیل غیبت بیش از حد مجاز از کلاس حذف شد</a>");
 
