4cf9b9d6160c14f83894363cb039e5d58ccb6aec
reza <rm.biqarar@gmail.com>
2017-01-16 22:49:34 +0330

    users search bug fix

--- portal/users/list/model.php
+++ portal/users/list/model.php
@@ -78,6 +78,8 @@ class model extends main_model {
 					$father = trim($vsearch_split[2]);
 				}
 
+				$result->groupOpen();
+				
 				if($public && !$family && !$father)
 				{
 					
@@ -101,6 +103,8 @@ class model extends main_model {
 				{
 					$result->condition("and", "person.father", "LIKE", "'%$father%'");
 				}
+				
+				$result->groupClose();
 
 				// echo ($result->select()->string());
 				// exit();
@@ -111,7 +115,6 @@ class model extends main_model {
 				// foreach ($search as $key => $value) {
 				// 	$search[$key] = "IFNULL($value, '')";
 				// }
-				// $result->groupOpen();
 				// $result->condition("and", "##concat($search)", "LIKE", "%$vsearch%");
 				// foreach ($csearch as $key => $value) {
 				// 	if(isset($ssearch[$key])){
@@ -123,7 +126,6 @@ class model extends main_model {
 				// 		}
 				// 	}
 				// }
-				// $result->groupClose();
 				// 
 				// ////////////////////////////////////
 				// 
