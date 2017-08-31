8bc2b3cf71e7ecfdff634e14b9ed263bb40eb397
Reza <rm.biqarar@gmail.com>
2017-08-31 09:48:38 +0430

    BUG FIX :|

--- cls/global.cls.php
+++ cls/global.cls.php
@@ -1,66 +1,82 @@
 <?php
-class global_cls{
+class global_cls
+{
 
-	static function supervisor(){
+	static function supervisor()
+	{
 		if(isset($_SESSION['user']['permission']['tables']['branch']['condition'])
-		&&  $_SESSION['user']['permission']['tables']['branch']['condition'] = "*"){
+		&&  $_SESSION['user']['permission']['tables']['branch']['condition'] == "*")
+		{
 			return true;
 		}
 		return false;
 	}
 
-	static function superprice($condition = false) {
-		
-		if(isset($_SESSION['user']['permission']['tables']['price']['condition'])){
-			
+	static function superprice($condition = false)
+	{
+
+		if(isset($_SESSION['user']['permission']['tables']['price']['condition']))
+		{
+
 			$price = $_SESSION['user']['permission']['tables']['price']['condition'];
 
-			switch ($condition) {
+			switch ($condition)
+			{
 				case 'classification':
-					if(isset($price['classification'])){
+					if(isset($price['classification']))
+					{
 						return true;
 					}
 					break;
 
 				case 'rule':
-					if(isset($price['rule'])){
+					if(isset($price['rule']))
+					{
 						return true;
 					}
 					break;
 
 				default:
-					if($price == "*"){
+					if($price == "*")
+					{
 						return true;
 					}
 					break;
 			}
-			if($price == "*"){
+			if($price == "*")
+			{
 				return true;
 			}
 		}
 		return self::supervisor();
 	}
 
-	static function superperson() {
+	static function superperson()
+	{
 		if(isset($_SESSION['user']['permission']['tables']['person']['condition'])
-		&&  $_SESSION['user']['permission']['tables']['person']['condition'] = "*"){
+		&&  $_SESSION['user']['permission']['tables']['person']['condition'] == "*")
+		{
 			return true;
 		}
 		return self::supervisor();
 	}
-	
 
-	static function superclassification() {
+
+	static function superclassification()
+	{
 		if(isset($_SESSION['user']['permission']['tables']['classification']['condition'])
-		&&  $_SESSION['user']['permission']['tables']['classification']['condition'] = "*"){
+		&&  $_SESSION['user']['permission']['tables']['classification']['condition'] == "*")
+		{
 			return true;
 		}
 		return self::supervisor();
 	}
 
-	static function supercertification() {
+	static function supercertification()
+	{
 		if(isset($_SESSION['user']['permission']['tables']['certification']['condition'])
-		&&  $_SESSION['user']['permission']['tables']['certification']['condition'] = "*"){
+		&&  $_SESSION['user']['permission']['tables']['certification']['condition'] == "*")
+		{
 			return true;
 		}
 		return self::supervisor();
